066700bab4fa58bd8f9ae45416e402f02f209c1d
diegodovale21 <diegodovale21@gmail.com>
2022-08-20 16:03:59 -0300

    first commit

---
+++ jogos.py
@@ -0,0 +1,23 @@
+import forca
+import adivinhacao
+
+
+def escolha_jogo():
+    print("**************************************")
+    print("***********Escolha seu jogo***********")
+    print("**************************************")
+
+    print('(1) Forca (2) Adivinhação')
+
+    jogo = int(input('Qual jogo?'))
+
+    if(jogo == 1):
+        print('Jogando Forca!')
+        forca.jogar()
+    elif(jogo == 2):
+        print('Jogando Adivinhação!')
+        adivinhacao.jogar()
+
+
+if(__name__ == '__main__'):
+    escolha_jogo()
